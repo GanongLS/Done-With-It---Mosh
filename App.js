247864b9171fb9f29e2b631a4c84210a55492e6f
Luki Subandi <ganlukisubandi@gmail.com>
2022-12-13 20:35:16 +0700

    update stylesheet

--- App.js
+++ App.js
@@ -1,121 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import {View, Text, StatusBar, SafeAreaView, StyleSheet} from 'react-native';
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const handlePress = () => console.log('pressed');
+  // const 
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
+    <SafeAreaView styles={styles.container}>
+      <Text numberOfLines={1} style={styles.centeredText} onPress={handlePress}>
+        Crazy App that will keep our mind sane. Magna sit aliqua anim officia
+        dolor velit cillum aute id culpa commodo in ipsum. Ullamco eiusmod
+        aliqua anim magna eu cillum. Dolore labore enim ex duis aliquip veniam
+        cillum officia nulla deserunt id consectetur. Enim deserunt mollit
+        fugiat consectetur ut sint ea minim quis Lorem Lorem exercitation velit.
+        Ea adipisicing eiusmod anim exercitation do culpa officia eu Lorem.
+        Laboris duis est laboris labore sunt et cupidatat ad fugiat anim et
+        eiusmod sunt eu. Minim nostrud non enim eu ea minim cillum cillum
+        excepteur.
       </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  let x = 1;
-  x.toString();
-  parseInt(x);
-  // x.filter()
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 };
 
+export default App;
+
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flexGrow: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  centeredText: {
+    alignSelf: 'center',
   },
 });
 
-export default App;
+const some = StyleSheet.create({});
